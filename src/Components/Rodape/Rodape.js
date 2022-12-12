a0566e0a6cf01c4e307a1f413f944a101ec03eb0
Ranbut <Ranbut_Beater@protonmail.com>
2022-12-12 12:09:03 -0300

    Teste

--- src/Components/Rodape/Rodape.js
+++ src/Components/Rodape/Rodape.js
@@ -1,25 +1,59 @@
-import {StyledNavbottom, Titulo, FilmeCard} from './styles';
-
-function Rodape({sessao}) {
+import styled from 'styled-components';
+export default function Rodape({sessao}) {
     if(sessao.hasOwnProperty('movie')){
         return(
-            <StyledNavbottom data-test="footer">
-                <FilmeCard>
+            <ContainerRodape data-test="footer">
+                <CardFilme>
                     <img src={sessao.movie.posterURL} alt={sessao.movie.title} />
-                </FilmeCard>
+                </CardFilme>
                 <Titulo>{sessao.movie.title}<br/>{sessao.day.weekday} - {sessao.name}</Titulo>
-            </StyledNavbottom>
+            </ContainerRodape>
         );
     }else{
         return(
-            <StyledNavbottom data-test="footer">
-                <FilmeCard>
+            <ContainerRodape data-test="footer">
+                <CardFilme>
                     <img src={sessao.posterURL} alt={sessao.title} />
-                </FilmeCard>
+                </CardFilme>
                 <Titulo>{sessao.title}</Titulo>
-            </StyledNavbottom>
+            </ContainerRodape>
         );
     }
 }
 
-export default Rodape;
+const ContainerRodape = styled.div`
+    position: fixed;
+    width: 100%;
+    height: 117px;
+    left: 0;
+    bottom: 0;
+    background-color: #DFE6ED;
+    border: 1px solid #9EADBA;
+    display: flex;
+    align-items: center;
+    padding-left: 10px;
+`;
+const Titulo = styled.div`
+    width: 280px;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 26px;
+    line-height: 30px;
+    color: #293845;
+`;
+const CardFilme = styled.div`
+    width: 64px;
+    height: 89px;
+    background-color: #FFFFFF;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+    border-radius: 2px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 15px;
+    img{
+        width: 48px;
+        height: 72px;
+    }
+`;
